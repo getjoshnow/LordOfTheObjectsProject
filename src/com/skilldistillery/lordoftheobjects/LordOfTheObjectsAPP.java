ea497b6732b86df120c28629c1baf22c8cf89f72
Josh <robbins@Joshs-MacBook-Pro.local>
2019-07-19 17:17:43 -0600

    Initial Commit

---
+++ src/com/skilldistillery/lordoftheobjects/LordOfTheObjectsAPP.java
@@ -0,0 +1,12 @@
+package com.skilldistillery.lordoftheobjects;
+
+public class LordOfTheObjectsAPP {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello world");
+		
+		
+	}
+
+}
